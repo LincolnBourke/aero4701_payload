3033e0a8860c26793e71ac0d64189ca3939a9984
ollieturner <otur3695@uni.sydney.edu.au>
2026-06-12 10:52:27 +1000

    Increased cropping of spatial histograms

--- apps/camera/tests/crop.py
+++ apps/camera/tests/crop.py
@@ -9,8 +9,6 @@ print(f"Original size: {img.shape[1]}x{img.shape[0]}")
 
 # Crop: y1:y2, x1:x2
 # x1, y1, x2, y2 = 140, 80, 500, 400 # good for camera min
-# x1, y1, x2, y2 = 100, 50, 560, 430
-# x1, y1, x2, y2 = 80, 40, 540, 440
 # x1, y1, x2, y2 = 120, 40, 540, 450 # good for far
 x1, y1, x2, y2 = 80, 20, 580, 480 # good for close (use for now)
 cropped = img[y1:y2, x1:x2]
@@ -18,6 +16,8 @@ cropped = img[y1:y2, x1:x2]
 print(f"Cropped size:  {cropped.shape[1]}x{cropped.shape[0]}")
 
 cv.imwrite("apps/camera/tests/cropped.png", cropped)
+
+print(f"Cropped size: {cropped.shape[1]}x{cropped.shape[0]}")
 
 ratio = cropped.shape[1]*cropped.shape[0] / (640*480)
 
